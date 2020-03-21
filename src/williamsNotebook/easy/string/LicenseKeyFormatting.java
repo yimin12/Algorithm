2ccbd0e465df59eb6c3f46a2953a9ff940764980
yimin Huang <hymlaucs@gmail.com>
2020-03-20 22:31:58 -0400

    "a"

--- src/williamsNotebook/easy/string/LicenseKeyFormatting.java
+++ src/williamsNotebook/easy/string/LicenseKeyFormatting.java
@@ -14,6 +14,9 @@ package williamsNotebook.easy.string;
 *   at least one character. Furthermore, there must be a dash inserted between two groups and all 
 *   lowercase letters should be converted to uppercase.
 *   Given a non-empty string S and a number K, format the string according to the rules described above.
+*   Input: S = "5F3Z-2e-9-w", K = 4   Output:  "5F3Z-2E9W"
+*   Explanation:
+*   	The string S has been split into two parts, each part has 4 characters. Note that the two extra dashes are not needed and can be removed.
 */
 
 /**
